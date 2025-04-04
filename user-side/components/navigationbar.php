4e425e6e839941e5ecf70f950d04e39a82aec4b6
HelloNiel <Niel.ladica07@gmil.com>
2025-04-04 20:33:41 +0800

    navigation bar"

---
+++ user-side/components/navigationbar.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar sticky-top">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="#Swabe">SWABE APPAREL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                <li class="nav-item"><a class="nav-link link-hover" href="#Swabe">Home</a></li>
+                <li class="nav-item"><a class="nav-link link-hover" href="#Products">Products</a></li>
+                <li class="nav-item"><a class="nav-link link-hover" href="#mens-latest">New Shirts</a></li>
+                <li class="nav-item">
+                    <a class="nav-link icon-hover link-hover" href="/shoes">
+                        <span class="text">Shoes</span>
+                        <i class="fas fa-shoe-prints icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link icon-hover link-hover" href="/shirts">
+                        <span class="text">Shirts</span>
+                        <i class="fas fa-tshirt icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link icon-hover link-hover" href="/collection">
+                        <span class="text">Collection</span>
+                        <i class="fas fa-box icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-bs-toggle="dropdown">Pages</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item link-hover" href="/about-us">About Us</a></li>
+                        <li><a class="dropdown-item link-hover" href="/location">Location</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown">Account</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item link-hover" href="/manage-account">Manage Account</a></li>
+                        <li><a class="dropdown-item link-hover" href="/feedback">Give us Feedback</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item link-hover" href="/login"><i class="fas fa-sign-out-alt"></i> Log In</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<?php include('script.php'); ?>
---
+++ user-side/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/src/css/bootstrap.min.css">
+</head>
+<body>
+    <?php include('./components/navigationbar.php'); ?>
+
+    <?php include('./components/script.php'); ?>
+</body>
+</html>
