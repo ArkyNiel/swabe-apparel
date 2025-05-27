ead6ccc8ffcad6124503e2b998023510d787202e
ArkyNiel <niel.ladica07@gmail.com>
2025-05-27 20:04:43 +0800

    logout session

--- assets/js/products.php
+++ assets/js/products.php
@@ -3,177 +3,282 @@ $products = [
     [
         "id" => 1,
         "name" => "Classic White Tee",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 2,
         "name" => "Black Essential",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 3,
         "name" => "Striped Casual",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 4,
         "name" => "White Summer Collection",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 5,
         "name" => "Black Urban Style",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 6,
         "name" => "Pattern Mix",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 7,
         "name" => "White Premium",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 8,
         "name" => "Black Signature",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 9,
         "name" => "Striped Modern",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 10,
         "name" => "White Classic Plus",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 11,
         "name" => "Black Elite",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 12,
         "name" => "Striped Premium",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 13,
         "name" => "White Comfort Fit",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 14,
         "name" => "Black Urban Classic",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 15,
         "name" => "Striped Elegance",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 16,
         "name" => "White Designer Edition",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 17,
         "name" => "Black Modern Fit",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 18,
         "name" => "Striped Luxury",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 19,
         "name" => "White Slim Fit",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 20,
         "name" => "Black Premium Collection",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 21,
         "name" => "Striped Designer",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 22,
         "name" => "White Limited Edition",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 23,
         "name" => "Black Exclusive",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 24,
         "name" => "Striped Signature",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 25,
         "name" => "White Elite Collection",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 26,
         "name" => "Black Urban Premium",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 27,
         "name" => "Striped Contemporary",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 28,
         "name" => "White Modern Classic",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱599.00"
     ],
     [
         "id" => 29,
         "name" => "Black Signature Plus",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 30,
         "name" => "Striped Elite Series",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 31,
         "name" => "White Premium Select",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 32,
         "name" => "Black Designer Series",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 33,
         "name" => "Striped Urban Elite",
-        "image" => "../assets/img/shirt3.jpg"
+        "image" => "../../assets/img/shirt3.jpg",
+        "color" => "Navy/White",
+        "size" => "S,M,L,XL",
+        "price" => "₱699.00"
     ],
     [
         "id" => 34,
         "name" => "White Exclusive Plus",
-        "image" => "../assets/img/shirt1.jpg"
+        "image" => "../../assets/img/shirt1.jpg",
+        "color" => "White",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ],
     [
         "id" => 35,
         "name" => "Black Premium Elite",
-        "image" => "../assets/img/shirt2.jpg"
+        "image" => "../../assets/img/shirt2.jpg",
+        "color" => "Black",
+        "size" => "S,M,L,XL",
+        "price" => "₱799.00"
     ]
 ];
 ?>
