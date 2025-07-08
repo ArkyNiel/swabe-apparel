904372958b71be467b8540c321a7e4c7d22a6665
HelloNiel <Niel.ladica07@gmil.com>
2025-07-08 18:55:25 +0800

    swabe banner updated

---
+++ user-side/components/swabe_apparel.php
@@ -0,0 +1,32 @@
+<div class="container-fluid p-0">
+  <div id="swabeCarousel" class="carousel slide carousel-fade w-100" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets/img/temp1.jpg" class="d-block w-100" alt="Banner 1" style="width: 100%; height: 88vh; object-fit: cover;">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/img/temp2.jpg" class="d-block w-100" alt="Shirt 1" style="width: 100%; height: 85vh; object-fit: cover;">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/img/temp3.jpg" class="d-block w-100" alt="Shoes" style="width: 100%; height: 85vh; object-fit: cover;">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/img/temp4.jpg" class="d-block w-100" alt="Shirt 2" style="width: 100%; height: 85vh; object-fit: cover;">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#swabeCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#swabeCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+
+<style>
+.carousel.carousel-fade .carousel-item {
+  transition: opacity 0.7s ease-in-out;
+}
+</style>
