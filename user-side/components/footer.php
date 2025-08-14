f1f7e4a26cef88bd2fa998bb897ce3e8d8aaa01f
HelloNiel <Niel.ladica07@gmil.com>
2025-08-14 23:29:16 +0800

    404 links fixed

--- user-side/components/footer.php
+++ user-side/components/footer.php
@@ -2,9 +2,9 @@
     <div class="container text-center">
         <span>&copy; <?php echo date('Y'); ?> Swabe Apparel. All rights reserved.</span>
         <br>
-        <a href="./links/privacy_policy.php" class="text-warning text-decoration-none mx-2" target="_blank">Privacy
+        <a href="../links/privacy_policy.php" class="text-warning text-decoration-none mx-2" target="_blank">Privacy
             Policy</a>
-        <a href="./links/terms_of_service.php" class="text-warning text-decoration-none mx-2" target="_blank">Terms of
+        <a href="../links/terms_of_service.php" class="text-warning text-decoration-none mx-2" target="_blank">Terms of
             Service</a>
     </div>
 </footer>
