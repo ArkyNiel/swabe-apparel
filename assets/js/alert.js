b1943c8323f6878c735c04a6873593d9977a801a
ArkyNiel <niel.ladica07@gmail.com>
2025-11-20 09:21:39 +0800

    login path

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -3,7 +3,7 @@ function showAlert(msg, type) {
     document.querySelectorAll('.alert').forEach(a => a.remove());
     const alert = document.createElement('div');
     alert.className = `alert alert-${type} alert-dismissible fade show position-fixed`;
-    alert.style.cssText = 'top:20px;right:20px;z-index:9999;min-width:300px;';
+    alert.style.cssText = 'top:20px;left:50%;transform:translateX(-50%);z-index:9999;min-width:300px;';
     alert.innerHTML = `${msg}<button class="btn-close" data-bs-dismiss="alert"></button>`;
     document.body.appendChild(alert);
     setTimeout(() => alert.remove(), 5000);
